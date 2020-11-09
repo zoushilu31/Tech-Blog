196b67661948f1082b922a8c61892c3aea1ed68a
shilu <zoushilu@gmail.com>
2020-11-08 20:25:17 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,46 +1,22 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-
-// Javascript Helpers
-const helpers = require('./utils/helpers');
-
-// Require Handlebars
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-
-// Use path to target static files
 const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers/');
 
-// Use Environment port or 3001
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Create a Session
-const session = require(`express-session`);
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-// Set Handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-// Connect Static Files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Load Database
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
